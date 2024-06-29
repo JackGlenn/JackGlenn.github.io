0373a15b9721765241d40f7051ecdc1e68fef36e
Jack <jglenn1038@gmail.com>
2024-06-29 19:33:02 -0400

    added header

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,25 @@ import vite from "/vite.svg"
 
 function App() {
   return (
-    <div className="contentGrid">
-      <div className="flexCol">
-        <PortfolioItem link={"a"} image={image} name={"first"} description={"=========================================================================="}/>
-        <PortfolioItem link={"a"} image={vite} name={"second"} description={"description"}/>
+    <>
+      <div className='header'>
+        <h1>Jack Glenn</h1>
+      </div>
+      <div className="contentGrid">
+        <div className="flexCol">
+          <PortfolioItem link={"a"} image={image} name={"first"} description={"=========================================================================="}/>
+          <PortfolioItem link={"a"} image={vite} name={"second"} description={"description"}/>
 
+        </div>
+        <div className="flexCol">
+          <PortfolioItem link={"a"} image={image} name={"third"} description={"description"}/>
+          <PortfolioItem link={"a"} image={image} name={"fourth"} description={"description"}/>
+        </div>
+        <div className="flexCol">
+          <PortfolioItem link={"a"} image={image} name={"fifth"} description={"description"}/>
+        </div>
       </div>
-      <div className="flexCol">
-        <PortfolioItem link={"a"} image={image} name={"third"} description={"description"}/>
-        <PortfolioItem link={"a"} image={image} name={"fourth"} description={"description"}/>
-      </div>
-      <div className="flexCol">
-        <PortfolioItem link={"a"} image={image} name={"fifth"} description={"description"}/>
-      </div>
-    </div>
+    </>
   )
 }
 
