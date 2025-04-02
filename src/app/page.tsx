602a2fa65754540996ad98af8b74bb608cb1e38f
Jack <jglenn1038@gmail.com>
2025-04-02 16:43:33 -0500

    neon hammer page completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,13 +11,13 @@ export default function Home() {
       </div>
       <div className="contentGrid">
         <div className="flexCol">
-          <PortfolioItem link={"NeonHammer"} image={"/JackOfHearts.png"} name={"NeonHammer"} description={"=========================================================================="}/>
+          <PortfolioItem link={"NeonHammer"} image={"/NeonHammer.png"} name={"NeonHammer"} description={"Fast paced action game built in Unity"}/>
           <PortfolioItem link={"a"} image={"/JackOfHearts.png"} name={"second"} description={"description"}/>
           <PortfolioItem link={"a"} image={"/test.jpg"} name={"third"} description={"description"}/>
 
         </div>
         <div className="flexCol">
-          <PortfolioItem link={"a"} image={"https://loremflickr.com/320/240"} name={"fourth"} description={"description"}/>
+          <PortfolioItem link={"a"} name={"fourth"} description={"description"}/>
           <PortfolioItem link={"a"} image={"/JackOfHearts.png"} name={"fifth"} description={"description"}/>
           <PortfolioItem link={"a"} name={"sixth"} description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit eveniet quas reprehenderit neque sit, amet voluptatibus cupiditate qui iste asperiores minima incidunt numquam eius, soluta rem officiis. Cum, reprehenderit tenetur!"}/>
         </div>
