ce11d9e5662f1e4b168c36ca353fba065d3e5b5f
Jack <jglenn1038@gmail.com>
2025-04-08 18:53:53 -0500

    operating systems basic page

--- src/app/OperatingSystem/page.tsx
+++ src/app/OperatingSystem/page.tsx
@@ -3,9 +3,10 @@ export default function OS() {
     <>
       <div className="projectPage">
         <h1>Operating System Components</h1>
-        <p></p>
-        <p></p>
-        <p></p>
+        <p>Operating system pager, file server, and thread library implemented in C++.</p>
+        <p>The OS pager manages memory by moving pages between physical memory, file space, and swap space. All transfers are done while translating between virtual and physical memory for every memory access by a program.</p>
+        <p>The file server manages accepts TCP requests allowing multiple users to create, delete, write, and read files with consistency and crash safety. </p>
+        <p>The thread library created an api of mutexes and conditional variables for the user to manage their threads. These features were implemented using a scheduler that swaps linux stack frames to change the current application being executed to balance execution of threads. The library also gracefully handles system interrupts.</p>
       </div>
     </>
   )
