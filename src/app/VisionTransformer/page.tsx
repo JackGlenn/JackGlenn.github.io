5dfac5ac65b38a473f0eda4560c5c099771db1c4
Jack <jglenn1038@gmail.com>
2025-04-09 19:13:50 -0500

    Vision transformer basic description added

--- src/app/VisionTransformer/page.tsx
+++ src/app/VisionTransformer/page.tsx
@@ -2,11 +2,15 @@ export default function VisionTransformer() {
   return (
     <>
       <div className="projectPage">
+        <img src={"/VisionTransformer.png"}/>
         <h1>Vision Transformer</h1>
-        <p></p>
-        <p></p>
-        <p></p>
+        <p>
+          This project was a comprehensive study and implementation of the Vision Transformer(Vit) architecture as proposed in the Google
+          paper from 2020. This project was undertaken to both understand the general transformer model and its use when
+          applied to the computer vision task of image classifcation. The transformer model itself was originally used
+          for natural language processing tasks and would later go on to be the base for models like ChatGPT.
+        </p>
       </div>
     </>
-  )
+  );
 }
